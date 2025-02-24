f420fd303c2442b358517673ba5d33f649454805
JakobLN2 <jakoblnibro@outlook.com>
2025-02-24 22:23:46 +0100

    small additions to implement vector class

--- homework/linear equations/main.cc
+++ homework/linear equations/main.cc
@@ -4,9 +4,19 @@
 
 
 int main() {
-    vector<double> a(3);
-    std::cout << a.size << std::endl;
-    // a[0] = 1; a[1] = 3; a[2] = 9;
+    vector<double> a(3),
+                   b(3);
+    std::cout << a.size << "\n";
+    a[0] = 1; a[1] = 3; a[2] = 9;
+    b[0] = 0; b[1] = 5; b[2] = 10;
+    std::cout << a[0] << ", " << a[1] << ", " << a[2] << "\n";
+    // a *= 2.0;
+    // std::cout << a[0] << ", " << a[1] << ", " << a[2] << "\n";
+    std::cout << b[0] << ", " << b[1] << ", " << b[2] << "\n";
+    
+    auto c = a + b;
+    // std::cout << c[0] << ", " << c[1] << ", " << c[2] << "\n";
+    a.print("a = ");
     // std::cout << a[0] << " " << a[1] << " " << a[2] << std::endl;
     // vector<double> b(3);
     // std::cout << a.size << " " << b.size << std::endl;
--- homework/linear equations/vector.h
+++ homework/linear equations/vector.h
@@ -6,34 +6,50 @@
 // #else
 //     #define NUMBER double
 // #endif
+#include"vector"
+#include<string>
+#include<iostream>
 
 template<typename T>
 class vector {
     // private:    
     public:
-        T data[];
-        int size = data.Length();
+        std::vector<T> data;
+        int size = data.size();
 
-    vector(int n) {data = new T[n];} // parametrized constructor
-    vector() = default; // default constructor
-    vector(const vector&)=default; // copy constructor
-    vector(vector&&)=default; // move constructor
-    ~vector()=default; // destructor
-    vector& operator=(const vector&)=default; // copy assignment
-    vector& operator=(vector&&)=default; // move assignment
+        vector(int n) : data(n) {} // parametrized constructor
+        vector() = default; // default constructor
+        vector(const vector&)=default; // copy constructor
+        vector(vector&&)=default; // move constructor
+        ~vector()=default; // destructor
+        vector& operator=(const vector&)=default; // copy assignment
+        vector& operator=(vector&&)=default; // move assignment
+        
+        T& operator[](int i) {return data[i];}
+        vector<T>& operator*=(double n);
+        
+        // void print(std::string s = "") const; //for debugging
+        void print(std::string s) const {
+            std::cout << s;
+            std::cout << "mojn gutter";
+            // for(T d : data) std::cout << d << ", ";
+            // for(int i = 0 ; i < size ; ++i) std::cout << data[i] << ", ";
+            std::cout << std::endl;
+        };
+        
+    };
+template<typename T>
+vector<T> operator+(const vector<T>&, const vector<T>&);
+// int size() return size;
+// const T& operator[](int i) const {return data[i];}
 
-    // int size() return size;
-    // T& operator[](int i) return data[i];
-    // const T& operator[](int i) const {return data[i];}
 
+// friend vector<T> operator+(const vector<T>&, const vector<T>&);
+// vector operator-(const vector&);
+// vector operator*(const vector&, double);
+// vector operator*(double, const vector&);
+// vector operator/(const vector&, double);
+// bool approx(const vector&, const vector&, double acc=1e-6,double eps=1e-6);
+// double dot(const vector&, const vector&);
 
-    // friend vector<T> operator+(const vector<T>&, const vector<T>&);
-    // vector operator-(const vector&);
-    // vector operator-(const vector&, const vector&);
-    // vector operator*(const vector&, double);
-    // vector operator*(double, const vector&);
-    // vector operator/(const vector&, double);
-    // bool approx(const vector&, const vector&, double acc=1e-6,double eps=1e-6);
-    // double dot(const vector&, const vector&);
-};
 #endif
