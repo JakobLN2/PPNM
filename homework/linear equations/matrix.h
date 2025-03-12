d35c2bfca9f8d6b430086c7b83a7499d358fba1f
Jakob LN <jakoblnibro@outlook.com>
2025-03-12 10:39:35 +0100

    git oopsie

---
+++ homework/linear equations/matrix.h
@@ -0,0 +1,69 @@
+#ifndef HAVE_matrix_H
+#define HAVE_matrix_H
+
+// #ifdef LONG_DOUBLE
+//     #define NUMBER long double
+// #else
+//     #define NUMBER double
+// #endif
+#include"vector.h"
+#include<vector>
+#include<string>
+#include<iostream>
+#include<stdexcept>
+
+class matrix {
+    // private:    
+    public:
+        std::vector<double> cols;
+        int nrows,
+            ncols;
+
+        matrix(int n, int m) : cols(m * n) {nrows = n; ncols = m;} // parametrized constructor
+        matrix() = default; // default constructor
+        matrix(const matrix&)=default; // copy constructor
+        matrix(matrix&&)=default; // move constructor
+        ~matrix()=default; // destructor
+        matrix& operator=(const matrix&)=default; // copy assignment
+        matrix& operator=(matrix&&)=default; // move assignment
+        
+        // void reshape(int n, int m) //TODO: implement reshape
+        // std::vector<double> getRow(int);
+        // vector getRow(int);
+        matrix getRow(int);
+        // void setRow(int, std::vector<double>);
+        // void setRow(int, vector);
+        void setRow(int, matrix);
+        void setRow(int, double);
+
+        // std::vector<double> getCol(int);
+        matrix getCol(int);
+        // void setCol(int, std::vector<double>);
+        void setCol(int, matrix);
+        void setCol(int, double);
+
+        // vector& operator[](int i) {return cols[i];}; //Has to be single indexing? a[1][2] - I dont have C++23?
+        double& operator()(int i, int j) {return cols[i + j * nrows];}; //double indexing
+        double operator()(int i, int j) const {return cols[i + j * nrows];};
+
+        matrix& operator*=(double);
+        matrix& operator/=(double);
+        matrix& operator+=(matrix&);
+        matrix& operator-=(matrix&);
+
+        void print(std::string s = "") const; //for debugging
+    };
+matrix operator+(const matrix& a, const matrix& b);
+matrix operator-(const matrix& a);
+matrix operator-(const matrix& a, const matrix& b);
+matrix operator/(const matrix& a, double);
+matrix operator*(const matrix& a, double);
+matrix operator*(double, const matrix& a);
+matrix operator*(const matrix& a, const matrix& b);
+matrix transpose(const matrix& a);
+
+// bool approx(const matrix& a, const matrix& b, double acc=1e-6,double eps=1e-6);
+void prod_compatible_exception(const matrix& a, const matrix& b); //Check if two matrices are compatible for matrix multiplication, if not throw an exception.
+void sum_compatible_exception(const matrix& a, const matrix& b); //Check if two matrices are compatible for addition (+ subtraction)
+
+#endif
---
+++ homework/linear equations/matrix_test.cc
@@ -0,0 +1,86 @@
+#include"vector.h"
+#include"matrix.h"
+#include<iostream>
+#include<string>
+#include<vector>
+
+
+int main() {
+    matrix a(2,3);
+    std::cout << "matrix a is a " << a.nrows << " x " << a.ncols << " matrix\n";
+    
+    //indexing
+    a(1,2) = 3.0;
+    std::cout << "a[1][2] = " << a(1,2) << "\n";
+    a.print("a = ");
+    
+    //getRow and setRow
+    a.setRow(1, 4.5);
+    a.print("a[1][:] = 4.5 = ");
+    matrix testRow = a.getRow(1);
+    testRow *= 2;
+    a.setRow(0, testRow);
+    a.print("a[0][:] = a[1][:] * 2 = ");
+
+    try {
+        matrix badRow(1,2);
+        badRow.print("badRow = ");
+        a.setRow(0, badRow);
+    } catch(std::invalid_argument& e) {std::cout << "cannot insert badRow into matrix a :)\n";}
+
+    matrix testCol = a.getCol(2);
+    testCol /= 8;
+    a.setCol(0, testCol);
+    a.print("a = ");
+
+
+    //     std::cout << "a[1][:] *= 2\n";
+//     a.print("a = ");
+
+//     std::cout << "a /= 3\n";
+//     a /= 3;
+//     a.print("a = ");
+
+//     std::cout << "a(1,1) = " << a(1,1) << ", a(0,2) = " << a(0,2) << "\n";
+
+//     matrix b(2,3);
+//     b.setRow(0, 9.0); b.setRow(1, 0.01);
+//     b.print("b = ");
+//     matrix c = a + b;
+//     c.print("c = a + b = ");
+//     c = a - b;
+//     c.print("c = a - b = ");
+//     c = b * 2;
+//     c.print("c = b * 2 = ");
+//     c = 2 * b;
+//     c.print("c = 2 * b = ");
+//     c = a / 2;
+//     c.print("c = a / 2 = ");
+//     transpose(b).print("transpose(b) = ");
+
+//     matrix d(2,2),
+//             e(2,2);
+//     d(0,0) = 1; d(1,1) = 1; d(1,0) = 0.5; d(0,1) = 0.5;
+//     e.setRow(0,42); e.setRow(1,8);
+//     d.print("d = ");
+//     e.print("e = ");
+//     matrix res = d * e;
+//     res.print("res = d * e = ");
+
+//     matrix f(2,3),
+//             g(3,1);
+//     f.setRow(0, 2.5); f(1,0) = 4; f(1,2) = 1;
+//     f.print("f = ");
+//     g(0,0) = 1; g(1,0) = 2; g(2,0) = 3;
+//     g.print("g = ");
+//     try{res = g * f;}
+//     catch(std::invalid_argument& e) {std::cout << "g * f is illegal :)\n";}
+//     res = f * g;
+//     res.print("res = f * g = ");
+
+
+
+
+
+    return 0;
+}
